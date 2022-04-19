4872a7d990b594c52dad89bf4119e73ec7e93bfc
Jose <jose.antonio.umeres@gmail.com>
2022-04-18 21:45:14 -0500

    Added handle login and handle logout functions

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 export const LoginScreen = () => {
+  
+  // permita navegar hacia una direccion
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // con el replace en true reemplaza la historia en lugar de crear una nueva entrada en la historia
+    // eso hace que cuando se seleccione la fecla de back no vuelva a la pagina de login porque ahora es una nueva historia
+    navigate('/marvel', { 
+      replace: true
+    });
+  }
+
   return (
-    <div>LoginScreen</div>
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr/>
+      <button 
+        class="btn btn-primary"
+        onClick={ handleLogin }
+      >
+          Login
+      </button>
+    </div>
   )
 }
