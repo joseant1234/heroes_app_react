89f93380eb3bc09ce86baeef672d66088d9e677e
Jose <jose.antonio.umeres@gmail.com>
2022-05-12 22:05:07 -0500

    Added another test to AppRouter

--- src/test/routes/AppRouter.test.js
+++ src/test/routes/AppRouter.test.js
@@ -5,24 +5,40 @@ import { AuthContext } from "../../auth/authContext";
 
 describe('Pruebas en <AppRouter />', () => {
 
-    const contextValue = {
-        user: {
-            logged: false
-        }
-    };
-
-
-
     test('debe de mostrar el login si no está autenticado', () => {
+
+        const contextValue = {
+            user: {
+                logged: false
+            }
+        };
 
         // no se usa el shallow xq este solo tendría hasta el nodo de BrowserRouter y no se vería lo q hay adentro
         const wrapper = mount(
             <AuthContext.Provider value={contextValue} >
                 <AppRouter />
             </AuthContext.Provider>
-        )
-
+        );
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('h1').text().trim()).toBe('Login')
+    })
+
+    test('debe de mostrar el componente de Marvel si está autenticado', () => {
+
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'Jose'
+            }
+        };
+
+        // no se usa el shallow xq este solo tendría hasta el nodo de BrowserRouter y no se vería lo q hay adentro
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue} >
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.navbar').exists()).toBeTruthy();
     })
 })
