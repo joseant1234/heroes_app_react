cd2ed0a880ed291d7b97cbec4dcdb9acf47fa07e
Jose <jose.antonio.umeres@gmail.com>
2022-05-15 17:15:19 -0500

    Added initialEntries to MemoryRouter

--- src/test/routes/DashboardRoutes.test.js
+++ src/test/routes/DashboardRoutes.test.js
@@ -13,12 +13,13 @@ describe('Pruebas en <DasboardRoutes />', () => {
         }
     }
 
-    test('debe de mostrarse correctamente', () => {
+    test('debe de mostrarse correctamente - Marvel', () => {
         // MemoryRouter permite hacer pruebas como si se estuviera en el navegador web
         // para este caso se usa el memoryRouter para que el useNavigation que se usa por ejemplo en el componente SearchScreen sea provisto
+        // dentro de initialEntries se coloca el segmento o el string de la url inicial, hacia donde va apuntar
         const wrapper = mount(
             <AuthContext.Provider value={contextValue}>
-                <MemoryRouter>
+                <MemoryRouter initialEntries={['/']}>
                     <DashboardRoutes />
                 </MemoryRouter>
             </AuthContext.Provider>
@@ -26,5 +27,19 @@ describe('Pruebas en <DasboardRoutes />', () => {
 
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('.text-info').text().trim()).toBe('Jose');
+        expect(wrapper.find('h1').text().trim()).toBe('MarvelScreen');
+    })
+
+    test('debe de mostrarse correctamente - DC', () => {
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/dc']}>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('h1').text().trim()).toBe('DcScreen');
     })
 })
