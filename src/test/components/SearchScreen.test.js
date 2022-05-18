1af0d1085834ba3422adc3e854cf7a63b90598e7
Jose <jose.antonio.umeres@gmail.com>
2022-05-18 15:50:52 -0500

    Added a test to useNavigate

--- src/test/components/SearchScreen.test.js
+++ src/test/components/SearchScreen.test.js
@@ -2,6 +2,15 @@ import { SearchScreen } from "../../components/search/SearchScreen"
 import { mount } from 'enzyme';
 import { MemoryRouter } from "react-router-dom";
 
+// tiene q tener el nombre mockNavigate para q funcione
+const mockNavigate = jest.fn();
+
+// cuando se importe react-router-dom se usa el objeto definido dentro de la funcion
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
+
 describe('Pruebas en <SearchScreen/> ', () => {
 
     test('debe de mostrarse correctamente con valores por defecto', () => {
@@ -37,5 +46,30 @@ describe('Pruebas en <SearchScreen/> ', () => {
         expect(wrapper.find('.alert-danger').text().trim()).toBe(`No hay resultados: batman123`);
     })
 
+    test('debe de llamar el navigate a la nueva pantalla', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        // simulate (evento a simular, los parametros del evento)
+        wrapper.find('input').simulate('change', {
+            target: {
+                name: 'searchText',
+                value: 'batman'
+            }
+        })
+
+        // tambien se puede hacer con simulate
+        // preventDefault: () => == preventDefault(){}
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        })
+        // lo q se necesita probar es q el navigate fue llamado con ciertos atributos, no q cambie la url porque eso ya lo han probado el equipo de react-router
+        // para probar q el navigate fue llamado se hace un mock
+
+        expect(mockNavigate).toHaveBeenCalledWith("?q=batman");
+    })
 
 })
