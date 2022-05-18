6046b6568c415445c9d840d5c9c99cb0db1b1ea0
Jose <jose.antonio.umeres@gmail.com>
2022-05-17 21:53:02 -0500

    Added more tests to SearchScreen

--- src/test/components/SearchScreen.test.js
+++ src/test/components/SearchScreen.test.js
@@ -8,12 +8,33 @@ describe('Pruebas en <SearchScreen/> ', () => {
 
         // se usa memoryrouter porque pide que usenavigate solo puede ser usado en el contexto del router
         const wrapper = mount(
-            <MemoryRouter initialEntries={['/']}>
+            <MemoryRouter initialEntries={['/search']}>
                 <SearchScreen/>
             </MemoryRouter>
         );
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un héroe');
+    })
+
+    test('debe de mostrar a Batman y el input con el valor del queryString', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('input').prop('value')).toBe('batman');
+    })
+
+    test('debe de mostrar un error si no se encuentra el hero', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('.alert-danger').text().trim()).toBe(`No hay resultados: batman123`);
     })
 
 
